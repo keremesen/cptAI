6b85d7ccd6d5aefa630268cd7b2c0a420a6606a9
Kerem Esen <kerem.esen.01@hotmail.com>
2023-07-06 02:22:34 +0300

    generate caption

---
+++ app/api/generate/route.ts
@@ -0,0 +1,136 @@
+import redis from "@/utils/redis";
+import requestIp from "request-ip";
+
+import { Ratelimit } from "@upstash/ratelimit";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { NextResponse } from "next/server";
+
+async function readStream(stream: any) {
+  // Get a reader
+  const reader = stream.getReader();
+
+  // A variable to store the final result
+  let result = "";
+
+  // A TextDecoder instance
+  const decoder = new TextDecoder();
+
+  // A loop to read the data
+  while (true) {
+    // Read a chunk
+    const { value, done } = await reader.read();
+
+    // If there is no more data, break the loop
+    if (done) {
+      break;
+    }
+
+    // If there is data, decode it and append it to the result
+    result += decoder.decode(value);
+  }
+
+  // Return the result
+  return result;
+}
+
+type Data = string;
+interface ExtendedNextApiRequest extends NextApiRequest {
+  body: {
+    imageUrl: string;
+  };
+}
+
+// Create a new ratelimiter, that allows 3 requests every 15 minutes
+const ratelimit = redis
+  ? new Ratelimit({
+    redis: redis,
+    limiter: Ratelimit.fixedWindow(3, "1440 m"),
+    analytics: true,
+  })
+  : undefined;
+
+async function handler(
+  req: ExtendedNextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  // Rate Limiter Code
+  // if (ratelimit) {
+  //   const identifier = requestIp.getClientIp(req);
+  //   const result = await ratelimit.limit(identifier!);
+  //   res.setHeader("X-RateLimit-Limit", result.limit);
+  //   res.setHeader("X-RateLimit-Remaining", result.remaining);
+
+
+  //   if (!result.success) {
+  //     res
+  //       .status(429)
+  //       .json(
+  //         "Too many uploads in 1 day. Please try again after 24 hours."
+  //       );
+  //     return;
+  //   }
+  // }
+
+  const data = await readStream(req.body);
+  // Parse the data into JSON
+  const imageUrl = JSON.parse(data).imageUrl;
+
+
+  let startResponse = await fetch("https://api.replicate.com/v1/predictions", {
+    method: "POST",
+
+    body: JSON.stringify({
+      version:
+        "2e1dddc8621f72155f24cf2e0adbde548458d3cab9f00c0139eea840d0ac4746",
+      input: {
+        image: imageUrl,
+        task: "image_captioning",
+      },
+    }),
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Token " + process.env.REPLICATE_API_KEY,
+      Accept: 'application/json',
+    },
+  });
+
+  let jsonStartResponse = await startResponse.json()
+  let endpointUrl = jsonStartResponse.urls.get
+
+
+  if (!endpointUrl) {
+    // Handle the case when the endpoint URL is missing
+    return new NextResponse(
+      JSON.stringify({ error: "Endpoint URL is missing" }),
+      { status: 500 }
+    );
+  }
+
+  // GET request to get the status of the image restoration process & return the result when it's ready
+  let caption: string | null = null;
+  while (!caption) {
+    // Loop in 1s intervals until the alt text is ready
+    console.log("polling for result...");
+    let finalResponse = await fetch(endpointUrl, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Token " + process.env.REPLICATE_API_KEY,
+      },
+    });
+    let jsonFinalResponse = await finalResponse.json();
+
+    if (jsonFinalResponse.status === "succeeded") {
+      caption = jsonFinalResponse.output;
+    } else if (jsonFinalResponse.status === "failed") {
+      break;
+    } else {
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+    }
+  }
+  let finalCaption: string = caption ? caption : "Failed"
+  return new NextResponse(finalCaption, { status: 200 })
+
+}
+export { handler as GET, handler as POST }
+
--- app/caption/page.tsx
+++ app/caption/page.tsx
@@ -1,13 +1,16 @@
 "use client";
 import { useState } from "react";
+import Image from "next/image";
 
 import Layout from "@/components/Layout";
 import ResizablePanel from "@/components/ResizablePanel";
+import LoadingDots from "@/components/LoadingDots";
 
 import { Uploader } from "uploader";
 import { UploadDropzone } from "react-uploader";
 
 import { AnimatePresence, motion } from "framer-motion";
+import { HiOutlineClipboardDocument } from "react-icons/hi2";
 
 const uploader = Uploader({
   apiKey: !!process.env.NEXT_PUBLIC_UPLOAD_API_KEY
@@ -42,14 +45,49 @@ const Caption = () => {
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
 
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(caption!);
+
+    setButtonText("Copied!");
+    setTimeout(() => {
+      setButtonText("Copy");
+    }, 2000);
+  };
+
+  async function generateCaption(fileUrl: string) {
+    await new Promise((resolve) => setTimeout(resolve, 500));
+    setLoading(true);
+    const res = await fetch("http://localhost:3000/api/generate", {
+      method: "POST",
+      body: JSON.stringify({ imageUrl: fileUrl }),
+      headers: {
+        "Content-Type": "application/json",
+        Accept: 'application/json',
+      },
+    });
+    if (res.status !== 200) {
+      const errorResponse = await res.text();
+      setError(errorResponse);
+    } else {
+      try {
+        const captionResponse = await res.text();
+        setCaption(captionResponse);
+      } catch (error) {
+        console.log(error);
+        setError("Invalid JSON response");
+      }
+    }
+    setLoading(false);
+  }
+
   const UploadDropZone = () => (
     <UploadDropzone
       uploader={uploader}
       options={options}
       onUpdate={(file) => {
         if (file.length !== 0) {
-         // setOriginalPhoto(file[0].fileUrl.replace("raw", "thumbnail"));
-          // generateCaption(file[0].fileUrl.replace("raw", "thumbnail"));
+          setOriginalPhoto(file[0].fileUrl.replace("raw", "thumbnail"));
+          generateCaption(file[0].fileUrl.replace("raw", "thumbnail"));
         }
       }}
       width="670px"
@@ -57,6 +95,7 @@ const Caption = () => {
     />
   );
 
+  //const caption2 = await generateCaption()
   return (
     <Layout>
       <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 mt-4 sm:mb-0 mb-8">
@@ -66,7 +105,81 @@ const Caption = () => {
         <ResizablePanel>
           <AnimatePresence>
             <motion.div className="flex justify-between items-center w-full flex-col mt-4">
-              {<UploadDropZone />}
+              {!originalPhoto && <UploadDropZone />}
+              {originalPhoto && !caption && (
+                <Image
+                  alt="original photo"
+                  src={originalPhoto}
+                  className="rounded-2xl"
+                  width={475}
+                  height={475}
+                />
+              )}
+              {caption && originalPhoto && (
+                <div className="flex sm:space-x-10 sm:flex-row flex-col text-white">
+                  <div className="w-full md:w-1/2">
+                    <h2 className="mb-1 font-medium text-lg">Original Photo</h2>
+                    <Image
+                      alt="original photo"
+                      src={originalPhoto}
+                      className="rounded-2xl relative"
+                      width={475}
+                      height={475}
+                    />
+                  </div>
+                  <div className="w-full md:w-1/2 sm:mt-0 mt-8">
+                    <h2 className="mb-1 font-medium text-lg ">
+                      Generated Caption
+                    </h2>
+                    <textarea
+                      className="w-full h-32 p-2 text-gray-700 border border-gray-300 rounded-md resize-none focus:outline-none focus:ring"
+                      placeholder="Enter text here..."
+                      value={caption.replace("Caption: ", "")}
+                    />
+                    <button
+                      className="px-4 py-2 text-white bg-[#5a5cd1] rounded-md hover:bg-[#3f4194]  focus:outline-none focus:ring"
+                      onClick={copyToClipboard}
+                    >
+                      <div className="flex flex-row items-center justify-center">
+                        <HiOutlineClipboardDocument className="w-5 h-5 mr-1" />
+                        {buttonText}
+                      </div>
+                    </button>
+                  </div>
+                </div>
+              )}
+              {loading && (
+                <button
+                  disabled
+                  className="bg-white rounded-full text-white font-medium px-4 pt-2 pb-3 mt-8 hover:bg-white/80 w-40"
+                >
+                  <span className="pt-4">
+                    <LoadingDots color="black" style="large" />
+                  </span>
+                </button>
+              )}
+              {error && (
+                <div
+                  className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded-xl mt-8"
+                  role="alert"
+                >
+                  <span className="block sm:inline">{error}</span>
+                </div>
+              )}
+              <div className="flex space-x-2 justify-center">
+                {originalPhoto && !loading && (
+                  <button
+                    onClick={() => {
+                      setOriginalPhoto(null);
+                      setCaption(null);
+                      setError(null);
+                    }}
+                    className="bg-white rounded-full text-black font-medium px-4 py-2 mt-8 hover:bg-white/80 transition"
+                  >
+                    Upload New Photo
+                  </button>
+                )}
+              </div>
             </motion.div>
           </AnimatePresence>
         </ResizablePanel>
